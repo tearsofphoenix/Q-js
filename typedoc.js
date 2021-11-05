f42a3cd86f6b91982262c68d0a91ecfcb6d95512
isaac.liu <isaac.liu@ringcentral.com>
2021-11-05 17:24:53 +0800

    update doc.

---
+++ typedoc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "entryPoints": [
+        "src/index.ts"
+    ],
+    "exclude": [
+        "**/__tests__/**/*"
+    ],
+    "excludeExternals": true,
+    "excludePrivate": true,
+    "out": "docs"
+}
